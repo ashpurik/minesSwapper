130cdefb77903e9107d42b2320ab91f15741e9b5
Anastasia Shpurik <ashpurik@mines.edu>
2014-04-30 22:55:09 -0600

    attempted to fix image upload

--- app/uploaders/item_image_uploader.rb
+++ app/uploaders/item_image_uploader.rb
@@ -14,7 +14,7 @@ class ItemImageUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
   #  "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    "uploads/items"
+    "#{Rails.root}/tmp/uploads"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
